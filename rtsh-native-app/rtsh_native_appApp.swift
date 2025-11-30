78fc1498f265d36981e29e8af32e1d049cf91ec7
maksim.sigov <maksim.sigov@eraneos.com>
2025-11-30 02:42:05 +0100

    Initial Commit

---
+++ rtsh-native-app/rtsh_native_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  rtsh_native_appApp.swift
+//  rtsh-native-app
+//
+//  Created by Maksim SIgov on 30.11.25.
+//
+
+import SwiftUI
+
+@main
+struct rtsh_native_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
